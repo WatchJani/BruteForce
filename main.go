1d3b13898e96a486ec4b924bb41e7b5655ba461c
WatchJani <jankokondic84@gmail.com>
2024-10-29 15:04:52 +0100

    work

--- main.go
+++ main.go
@@ -13,17 +13,23 @@ func BruteForce(data *[]byte) {
 	block := make([]byte, 1, 4)
 	indexSaver := make([]int, 1, 4)
 
-	for index := 0; index < 8; index++ {
-		if indexSaver[0] > len(list)-1 {
-			block = append(block, list[0]) //Dont touch
-			indexSaver[0] = 0
-			indexSaver = append(indexSaver, 0) //Dont touch
+	for index := 0; index < 12; index++ {
+		for f := 0; indexSaver[f] > len(list)-1; f++ {
+			if len(indexSaver) > f+1 {
+				indexSaver[f+1]++
+				block[f+1] = list[indexSaver[f+1]]
+				indexSaver[f] = 0
+			} else {
+				block = append(block, list[0]) //Dont touch
+				indexSaver[f] = 0
+				indexSaver = append(indexSaver, 0) //Dont touch
+			}
 		}
 
 		block[0] = list[indexSaver[0]]
 
 		indexSaver[0]++
-		
+
 		fmt.Println(string(block))
 	}
 }
