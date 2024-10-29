fa7f8e8f7980facb4fcddf6c159753fa8c74c14c
WatchJani <jankokondic84@gmail.com>
2024-10-29 19:42:14 +0100

    work

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
-import "fmt"
-
 func main() {
-	fmt.Println(BruteForceChatGPT("Janko"))
+	Combination(1254)
+	// fmt.Println(BruteForceChatGPT("Janko"))
+}
+
+func Combination(code int) {
+	
 }
 
 func BruteForce(password string) string {
@@ -15,8 +18,8 @@ func BruteForce(password string) string {
 	for {
 		for f := 0; indexSaver[f] > len(list)-1; f++ {
 			indexSaver[f] = 0
-			if len(indexSaver) > f+1 { //increase first next character
 
+			if len(indexSaver) > f+1 { //increase first next character
 				indexSaver[f+1]++
 				if indexSaver[f+1] > len(list)-1 {
 					block[f+1] = list[0]
@@ -26,8 +29,8 @@ func BruteForce(password string) string {
 				block[f+1] = list[indexSaver[f+1]]
 			} else { //add new character
 
-				block = append(block, list[0])     //Dont touch
-				indexSaver = append(indexSaver, 0) //Dont touch
+				block = append(block, list[0])
+				indexSaver = append(indexSaver, 0)
 			}
 		}
 
@@ -37,39 +40,6 @@ func BruteForce(password string) string {
 
 		if string(block) == password {
 			return string(block)
-		}
-	}
-}
-
-func BruteForceChatGPT(password string) string {
-	list := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
-
-	block := make([]byte, 1, 4)
-	indexSaver := make([]int, 1, 4)
-
-	for {
-		for i := range block {
-			block[i] = list[indexSaver[i]]
-		}
-
-		if string(block) == password {
-			return string(block)
-		}
-
-		indexSaver[0]++
-
-		for i := 0; i < len(indexSaver); i++ {
-			if indexSaver[i] < len(list) {
-				break
-			}
-
-			indexSaver[i] = 0
-			if i+1 < len(indexSaver) {
-				indexSaver[i+1]++
-			} else {
-				indexSaver = append(indexSaver, 0)
-				block = append(block, list[0])
-			}
 		}
 	}
 }
--- main_test.go
+++ main_test.go
@@ -6,14 +6,9 @@ import "testing"
 // 6778759411 ns/op
 func BenchmarkMyFn(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		BruteForce("Janko")
+		BruteForce("JankoKondic")
 	}
 }
 
 // 8.635s
 // 8632277364 ns/op
-func BenchmarkGPT(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		BruteForceChatGPT("Janko")
-	}
-}
