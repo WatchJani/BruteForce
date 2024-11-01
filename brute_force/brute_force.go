edd06ee8c8f37b84aaeb82e9914008c344253f69
WatchJani <jankokondic84@gmail.com>
2024-11-01 17:39:11 +0100

    server

---
+++ brute_force/brute_force.go
@@ -0,0 +1,115 @@
+package brute_force
+
+const Characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
+
+type BruteForce struct {
+	dataStreamCh chan DataStream
+	responseCh   chan string
+}
+
+func New() *BruteForce {
+	return &BruteForce{
+		dataStreamCh: make(chan DataStream),
+		responseCh:   make(chan string),
+	}
+}
+
+func (bf *BruteForce) Send(dataStream DataStream) {
+	bf.dataStreamCh <- dataStream
+}
+
+type DataStream struct {
+	hash       string //password
+	startPoint []int
+}
+
+func NewDataStream(hash string, startPoint []int) DataStream {
+	return DataStream{
+		hash:       hash,
+		startPoint: startPoint,
+	}
+}
+
+func FindCombination(position int) []int {
+	if position == 0 {
+		return []int{0}
+	}
+
+	alphabetSize := len(Characters)
+
+	position -= 1
+
+	var length int
+	for {
+		numCombinations := pow(alphabetSize, length+1)
+		if position < numCombinations {
+			break
+		}
+		position -= numCombinations
+		length++
+	}
+
+	result := make([]int, length+1)
+
+	for i := length; i >= 0; i-- {
+		result[i] = position % alphabetSize
+		position /= alphabetSize
+	}
+
+	for index := 0; index < len(result)/2; index++ {
+		result[index], result[len(result)-1-index] = result[len(result)-1-index], result[index]
+	}
+
+	return result
+}
+
+func pow(a, b int) int {
+	result := 1
+	for b > 0 {
+		result *= a
+		b--
+	}
+	return result
+}
+
+// 10_000_000_000
+func (bf *BruteForce) Worker(workerIndex int) {
+	var index int
+
+	for {
+		store := <-bf.dataStreamCh
+
+		indexSaver := store.startPoint
+		block := make([]byte, len(indexSaver))
+		for index, value := range indexSaver {
+			block[index] = Characters[value]
+		}
+
+		for ; index < 10_000_000_000; index++ {
+			for f := 0; indexSaver[f] > len(Characters)-1; f++ {
+				indexSaver[f] = 0
+
+				if len(indexSaver) > f+1 { //increase first next character
+					indexSaver[f+1]++
+					if indexSaver[f+1] > len(Characters)-1 {
+						block[f+1] = Characters[0]
+						continue
+					}
+
+					block[f+1] = Characters[indexSaver[f+1]]
+				} else { //add new character
+					block = append(block, Characters[0])
+					indexSaver = append(indexSaver, 0)
+				}
+			}
+
+			block[0] = Characters[indexSaver[0]]
+
+			indexSaver[0]++
+
+			if string(block) == store.hash {
+				bf.responseCh <- string(block)
+			}
+		}
+	}
+}
--- main.go
+++ main.go
@@ -1,147 +1,65 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"os/signal"
+	"root/brute_force"
+	s "root/server"
 	"runtime"
-	"syscall"
-	"time"
 )
 
-const Characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
+func Exist(c *s.Ctx) {
+	c.ResWriter("cmd: exist\n")
+}
+
+func (n *Node) Cancel(c *s.Ctx) {
+	n.processController <- struct{}{}
+	c.ResWriter("cmd: info\n Msg:\r Process is cancel!\n")
+}
+
+func (n *Node) Start(c *s.Ctx) {
+	// mod := c.GetHeader()["mod"]
+loop:
+	for {
+		select {
+		case <-n.processController:
+			break loop
+		default:
+			for index := 0; index*10_000_000_000 < 1_000_000_000_000; index++ {
+				n.Send(brute_force.NewDataStream("JankoKondic", brute_force.FindCombination(index*10_000_000_000)))
+			}
+		}
+	}
+
+	n.work = false
+}
+
+type Node struct {
+	*brute_force.BruteForce
+	processController chan struct{}
+	work              bool
+}
+
+func NewNode(brutForce *brute_force.BruteForce) Node {
+	return Node{
+		BruteForce:        brutForce,
+		processController: make(chan struct{}),
+	}
+}
 
 func main() {
-	sigs := make(chan os.Signal, 1)
-	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
-
-	bf := New()
-	start := time.Now()
+	//system part
+	bf := brute_force.New()
+	node := NewNode(bf)
 
 	for index := range runtime.NumCPU() {
 		go bf.Worker(index)
 	}
 
-	//controlling system closing
-	go func() {
-		<-sigs
+	//protocol part
+	mux := s.NewRouter()
 
-		fmt.Println(time.Since(start))
-		os.Exit(1)
-	}()
+	mux.HandleFunc("start", node.Start)
+	mux.HandleFunc("cancel", node.Cancel)
+	mux.HandleFunc("exist", Exist)
 
-	//2m22s
-	go func() {
-		for index := 0; index*10_000_000_000 < 1_000_000_000_000; index++ {
-			fmt.Println(index * 10_000_000_000)
-			bf.dataStreamCh <- DataStream{
-				hash:       "JankoKondic",
-				startPoint: findCombination(index * 10_000_000_000),
-			}
-		}
-
-		fmt.Println(time.Since(start))
-		os.Exit(1)
-	}()
-
-	fmt.Println(<-bf.responseCh)
-}
-
-type BruteForce struct {
-	dataStreamCh chan DataStream
-	responseCh   chan string
-}
-
-func New() BruteForce {
-	return BruteForce{
-		dataStreamCh: make(chan DataStream),
-		responseCh:   make(chan string),
-	}
-}
-
-type DataStream struct {
-	hash       string //password
-	startPoint []int
-}
-
-func findCombination(position int) []int {
-	if position == 0 {
-		return []int{0}
-	}
-
-	alphabetSize := len(Characters)
-
-	position -= 1
-
-	var length int
-	for {
-		numCombinations := pow(alphabetSize, length+1)
-		if position < numCombinations {
-			break
-		}
-		position -= numCombinations
-		length++
-	}
-
-	result := make([]int, length+1)
-
-	for i := length; i >= 0; i-- {
-		result[i] = position % alphabetSize
-		position /= alphabetSize
-	}
-
-	for index := 0; index < len(result)/2; index++ {
-		result[index], result[len(result)-1-index] = result[len(result)-1-index], result[index]
-	}
-
-	return result
-}
-
-func pow(a, b int) int {
-	result := 1
-	for b > 0 {
-		result *= a
-		b--
-	}
-	return result
-}
-
-// 10_000_000_000
-func (bf *BruteForce) Worker(workerIndex int) {
-	for {
-		store := <-bf.dataStreamCh
-
-		indexSaver := store.startPoint
-		block := make([]byte, len(indexSaver))
-		for index, value := range indexSaver {
-			block[index] = Characters[value]
-		}
-
-		for index := 0; index < 10_000_000_000; index++ {
-			for f := 0; indexSaver[f] > len(Characters)-1; f++ {
-				indexSaver[f] = 0
-
-				if len(indexSaver) > f+1 { //increase first next character
-					indexSaver[f+1]++
-					if indexSaver[f+1] > len(Characters)-1 {
-						block[f+1] = Characters[0]
-						continue
-					}
-
-					block[f+1] = Characters[indexSaver[f+1]]
-				} else { //add new character
-					block = append(block, Characters[0])
-					indexSaver = append(indexSaver, 0)
-				}
-			}
-
-			block[0] = Characters[indexSaver[0]]
-
-			indexSaver[0]++
-
-			if string(block) == store.hash {
-				bf.responseCh <- string(block)
-			}
-		}
-	}
+	s.ListenAndServe(":5000", mux)
 }
